395b5d25df93ffab081e007ec73d8827a5d663cc
Siddhant Tyagi <siddhanttyagi432@gmail.com>
2024-08-23 22:56:15 +0530

    testing the json data with LLM api with prompt

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ api_key = os.getenv('api_key')
 
 genai.configure(api_key= api_key)
 
+model = genai.GenerativeModel('gemini-1.5-flash',
+                              # Set the `response_mime_type` to output JSON
+                              generation_config={"response_mime_type": "application/json"})
+
+prompt = """
+  List 5 popular cookie recipes.
+  Using this JSON schema:
+    Recipe = {"recipe_name": str}
+  Return a `list[Recipe]`
+  """
+
+response = model.generate_content(prompt)
+print(response.text)
