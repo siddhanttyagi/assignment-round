76f1a73d3dcd0b03ff6384ea2d234d692b794385
Siddhant Tyagi <siddhanttyagi432@gmail.com>
2024-08-24 15:24:12 +0530

    final-code-commited-with-output-json-file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 import os
 import json
+import time
 
 load_dotenv()
 
@@ -13,12 +14,16 @@ model = genai.GenerativeModel('gemini-1.5-flash',
                               # Set the `response_mime_type` to output JSON
                               generation_config={"response_mime_type": "application/json"})
 
-with open('temp.json', 'r', encoding='utf-8') as file:
+with open('data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
     
-
+res_list = []
+cnt=0
 for item in data:
-  
+  cnt+=1
+  if cnt==14:
+    print("Waiting for 1.5 minutes...")
+    time.sleep(90)
   prompt = """
     I have given you the json object whose schema explination is as follow :
   {
@@ -43,6 +48,8 @@ for item in data:
   3. `role` -> Defines the message sender(*User* is patient and *Dt Disha* is our careteam)
 
   Use the below json data, understand the messages in chat history and reply to the query of the patient. You can see the ideal_response for your reference.
+  Note- Don't generate the response exactly same the ideal_response. try to make it better. use the json data for it.
+  also, answer the query in the same style as query written in i.e if it in english answer it in english and same goes for Hindi, hinglish or any other language.
   in the output i want a json object which look like this:{"generated_response": "your_response"}
   also Note that: your_response is a string. please make Your_response small and to the point.
 
@@ -51,3 +58,22 @@ for item in data:
 
   response = model.generate_content(prompt)
   print(response.text)
+  response_dict = json.loads(response.text)
+  temp_dict={}
+  temp_dict['ticket_id'] = item['chat_context']['ticket_id']
+  temp_dict['latest_query'] = item['latest_query']
+  temp_dict['generated_response'] = response_dict['generated_response']
+  temp_dict['ideal_response'] = item['ideal_response']
+  
+  res_list.append(temp_dict)
+
+print('----------------------------------------------------------------')
+
+output_file = 'output.json'
+
+# Write the list to the JSON file
+with open(output_file, 'w') as file:
+    json.dump(res_list, file, indent=4)
+
+print(f"Data has been written to {output_file}")
+print(len(res_list))
